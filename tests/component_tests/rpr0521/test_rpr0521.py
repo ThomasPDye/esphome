d7787c6b4fd8d6c9c3339003be82295d8e69a695
ThomasPDye <tompdye@gmail.com>
2023-01-02 09:32:32 +0000

    corrections to test_rpr0521

--- tests/component_tests/rpr0521/test_rpr0521.py
+++ tests/component_tests/rpr0521/test_rpr0521.py
@@ -2,5 +2,5 @@
 
 
 def test_rpr0521_device_class_set(generate_main):
-    main_cpp = generate_main("test/component_tests/sensor/rpr0521/test_rpr0521.yaml")
+    main_cpp = generate_main("tests/component_tests/rpr0521/test_rpr0521.yaml")
     assert 's_1->set_device_class("distance");' in main_cpp
